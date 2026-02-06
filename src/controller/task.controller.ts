e0df3b18405b2b55cec1d582a2541be9460e4461
willianramosdev-stack <willianramos.dev@gmail.com>
2026-02-06 00:00:53 -0300

    feat: add task retrieval endpoint with filtering and sorting capabilities

--- src/controller/task.controller.ts
+++ src/controller/task.controller.ts
@@ -4,6 +4,7 @@ import { prisma } from "../client.js";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { authenticate } from "../utils/authenticate.js";
+import type { Prisma } from "../generated/prisma/client.js";
 
 export const TaskStatusSchema = z.enum([
     "PENDING",
@@ -25,6 +26,13 @@ export const CreateTaskSchema = z.object({
     dueDate: z.coerce.date().nullable() // usar coerce para converter string para data
 });
 
+const QuerySchema = z.object({
+    status: z.enum(["PENDING", "IN_PROGRESS", "DONE"]).optional(),
+    priority: z.enum(["LOW", "MEDIUM", "HIGH"]).optional(),
+    due_date: z.coerce.date().optional(),
+    sort: z.enum(["dueDate", "createdAt"]).optional().default("createdAt"),
+});
+
 export const taskController = async (fastify: FastifyInstance) => {
 
     fastify.post<{ Body: z.infer<typeof CreateTaskSchema> }>('/api/tasks', { preHandler: authenticate }, async (request, reply) => {
@@ -41,5 +49,34 @@ export const taskController = async (fastify: FastifyInstance) => {
             reply.status(500).send({ error: "Failed to create task" });
         }
     });
+
+    fastify.get<{ Querystring: z.infer<typeof QuerySchema>; }>('/api/tasks', { preHandler: authenticate }, async (request, reply) => {
+        try {
+            const query = QuerySchema.parse(request.query);
+
+            const filters: Prisma.TaskWhereInput = {
+                userId: request.user.userId,
+            }
+            if (query.status) {
+                filters.status = query.status;
+            }
+            if (query.priority) {
+                filters.priority = query.priority;
+            }
+            if (query.due_date) {
+                filters.dueDate = query.due_date;
+            }
+            const getAllTask = await prisma.task.findMany({
+                where: filters,
+                orderBy: {
+                    [query.sort]: 'desc'
+                }
+            });
+            reply.status(200).send(getAllTask);
+        } catch (error) {
+            reply.status(500).send({ error: "Tasks not found!" });
+        }
+    });
+
 
 }
